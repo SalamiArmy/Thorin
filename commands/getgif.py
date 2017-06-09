0a3741e906c24facdd3e98143132ae3f77b651c1
Salami Army <ultrakiff@gmail.com>
2017-06-09 21:21:45 +0200

    god damn it

--- commands/getgif.py
+++ commands/getgif.py
@@ -125,7 +125,7 @@ def search_results_walker(args, bot, chat_id, data, number, requestText, results
         if not wasPreviouslySeenGif(chat_id, imagelink):
             addPreviouslySeenGifsValue(chat_id, imagelink)
             if is_valid_gif(imagelink):
-                ImageTags = Image_Tags(imagelink, keyConfig)
+                ImageTags = get.Image_Tags(imagelink, keyConfig)
                 if retry_on_telegram_error.SendDocumentWithRetry(bot, chat_id, imagelink, requestText +
                         (' ' + str(total_sent + 1) + ' of ' + str(number) if int(number) > 1 else '') +
                         (' (I see' + ImageTags + ')' if ImageTags != '' else '')):
