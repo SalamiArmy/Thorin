6676de1920ad8414342b54d2e0fc96f0a2608cd6
Salami Army <ultrakiff@gmail.com>
2017-06-10 08:18:40 +0200

    emergency. Google App Engine version is not updating. There are no errors logged. Emergency!

--- commands/getgif.py
+++ commands/getgif.py
@@ -136,3 +136,4 @@ def search_results_walker(args, bot, chat_id, data, number, requestText, results
         return search_results_walker(args, bot, chat_id, data, number, requestText, results_this_page, total_results, keyConfig,
                                      total_sent, total_offset)
     return int(total_sent)
+ 
