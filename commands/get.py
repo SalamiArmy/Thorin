e0c2fc271a5f7ab953d2d3a0fe54c1af0745366d
Salami Army <ultrakiff@gmail.com>
2017-06-18 22:42:14 +0200

    fiddling with vision tag formatting

--- commands/get.py
+++ commands/get.py
@@ -158,22 +158,22 @@ def Image_Tags(imagelink, keyConfig):
     if strAdult == 'POSSIBLE' or \
         strAdult == 'LIKELY' or \
         strAdult == 'VERY_LIKELY':
-        tags += strAdult.replace('_', ' ').lower() + " porn, "
+        tags += strAdult.replace('VERY_LIKELY', '').lower() + ' porn, '
     strViolence = visionData['responses'][0]['safeSearchAnnotation']['violence']
     if strViolence == 'POSSIBLE' or \
         strViolence == 'LIKELY' or \
         strViolence == 'VERY_LIKELY':
-        tags += strViolence.replace('_', ' ').lower() + " violence, "
+        tags += strViolence.replace('VERY_LIKELY', '').lower() + ' offensive violence, '
     strMedical = visionData['responses'][0]['safeSearchAnnotation']['medical']
     if strMedical == 'POSSIBLE' or \
         strMedical == 'LIKELY' or \
         strMedical == 'VERY_LIKELY':
-        tags += strMedical.replace('_', ' ').lower() + " medical, "
+        tags += strMedical.replace('VERY_LIKELY', '').lower() + ' graphic medical content warning, '
     strSpoof = visionData['responses'][0]['safeSearchAnnotation']['spoof']
     if strSpoof == 'POSSIBLE' or \
         strSpoof == 'LIKELY' or \
         strSpoof == 'VERY_LIKELY':
-        tags += strSpoof.replace('_', ' ').lower() + " spoof, "
+        tags += strSpoof.replace('VERY_LIKELY', '').lower() + ' offensive slure, '
     return tags.rstrip(', ')
 
 def Send_Images(bot, chat_id, user, requestText, args, keyConfig, number=1):
